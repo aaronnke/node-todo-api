e1d7b7a57ab6f3a1568c10202202e6a903dc3e11
Aaron Ng <aaronnke@gmail.com>
2019-02-13 00:23:12 +0800

    add get /users/me and auth middleware

--- server/models/user.js
+++ server/models/user.js
@@ -38,6 +38,21 @@ UserSchema.methods.toJSON = function() {
     return _.pick(userObject, ['_id', 'email'])
 }
 
+UserSchema.statics.findByToken = function(token) {
+    const User = this
+    let decoded
+    try {
+        decoded = jwt.verify(token, 'abc123')
+    } catch (e) {
+        return Promise.reject()
+    }
+    return User.findOne({
+        '_id': decoded._id,
+        'tokens.token': token,
+        'tokens.access': 'auth',
+    })
+}
+
 UserSchema.methods.generateAuthToken = function() {
     const user = this
     const access = 'auth'
